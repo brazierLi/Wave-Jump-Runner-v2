418bb53c6d8ce3518278059bec22a43ab5b2ca0f
brazierLi <liam.brazier1@gmail.com>
2017-01-21 12:56:16 +0000

    Main old
    Main new

---
+++ Assets/Script/Camer_Controller.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camer_Controller : MonoBehaviour {
+
+    public float yOffset = 5, zOffset = -10,xOffSEt = 0.6f;
+    GameObject Player;
+    void Start() {
+        Player = GameObject.Find("Player");
+    }
+
+
+	
+	// Update is called once per frame
+	void Update () {
+       GetComponent<Rigidbody2D>().velocity = Player.transform.position - transform.position + new Vector3(Player.GetComponent<Rigidbody2D>().velocity.x*xOffSEt, yOffset, zOffset); ;
+    }
+}
---
+++ Assets/Script/MoveCloser.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCloser : MonoBehaviour {
+
+    Player_Controller Player;
+
+    public string MoveCloserTag = "Damage";
+    public float RegularSpeed = 5,dist4OutOfRange = 15,outOfRange = 11, AddSpeed = 5;
+    float currentAddSpeed = 0;
+
+    Camera c;
+    private void Start()
+    {
+        c = GameObject.Find("Main Camera").GetComponent<Camera>();
+        dist4OutOfRange = -c.ScreenToWorldPoint(new Vector3(0, 0)).x;   
+    }
+
+
+    void FixedUpdate () {
+        currentAddSpeed = 0f;
+        Player = GameObject.Find("Player").GetComponent<Player_Controller>();
+        float dis = c.ScreenToWorldPoint(new Vector3(0, 0)).x - Player.gameObject.transform.position.x;
+        dist4OutOfRange = dis > 0 ? dis : -dis;
+        if (Player.CurrentHitTag == MoveCloserTag)
+        {
+            currentAddSpeed += AddSpeed;
+        }
+        if (Vector3.Distance( GameObject.Find("Player").transform.position, transform.position) > dist4OutOfRange)
+        {
+            currentAddSpeed += outOfRange;
+        }
+        else {
+            currentAddSpeed += RegularSpeed;
+        }
+        GetComponent<Rigidbody2D>().velocity = new Vector2(RegularSpeed + currentAddSpeed, 0);
+	}
+}
